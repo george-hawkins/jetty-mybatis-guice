6c24ca8330f4a307a445988d8391ae7629b194c1
George Hawkins <george-hawkins@users.noreply.github.com>
2015-01-05 19:06:19 +0100

    Created basic webapp.

---
+++ src/main/java/net/betaengine/secureiot/heroku/Main.java
@@ -0,0 +1,37 @@
+package net.betaengine.secureiot.heroku;
+
+import org.eclipse.jetty.server.Server;
+import org.eclipse.jetty.webapp.WebAppContext;
+import java.util.Optional;
+
+/**
+ * This class launches the web application in an embedded Jetty container.
+ */
+public class Main {
+
+    public static void main(String[] args) throws Exception{
+        // The port that we should run on can be set into an environment variable
+        // Look for that variable and default to 8080 if it isn't there.
+        final int webPort = Optional.ofNullable(System.getenv("PORT")).map(Integer::parseInt).orElse(8080);
+
+        final Server server = new Server(webPort);
+        final WebAppContext root = new WebAppContext();
+
+        root.setContextPath("/");
+        // Parent loader priority is a class loader setting that Jetty accepts.
+        // By default Jetty will behave like most web containers in that it will
+        // allow your application to replace non-server libraries that are part of the
+        // container. Setting parent loader priority to true changes this behavior.
+        // Read more here: http://wiki.eclipse.org/Jetty/Reference/Jetty_Classloading
+        root.setParentLoaderPriority(true);
+
+        final String webappDirLocation = "src/main/webapp/";
+        root.setDescriptor(webappDirLocation + "/WEB-INF/web.xml");
+        root.setResourceBase(webappDirLocation);
+
+        server.setHandler(root);
+
+        server.start();
+        server.join();
+    }
+}
